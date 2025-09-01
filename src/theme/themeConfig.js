61eecdfa7962e2499e212aba251442775e4e11e2
Shaikh Asjad <shaikhasjad29@gmail.com>
2025-09-01 18:29:32 +0530

    changes

--- src/theme/themeConfig.js
+++ src/theme/themeConfig.js
@@ -15,6 +15,10 @@ export const getThemeModules = (theme = "theme1") => {
         AccountsLayout: load(() => import("./theme1/Accounts/layout")),
         ProductDetail: load(() => import("./theme1/ProductDetail/single/ProductDetail")),
         WholeSaleProductList: load(() => import("./theme1/CategoriesPage/Wholesale/WholeSaleProductList")),
+         BrandList: load(() => import("./theme1/brands/brandLists")),
+         BrandCatalogueList: load(() => import("./theme1/brands/brandCatalogueList")),
+          Catalogue: load(() => import("./theme1/ProductDetail/catalogue/catalogue")),
+           WholeSalePage:load(() => import("./theme1/wholesalePage/wholesalepage"))
       };
 
     case "theme2":
@@ -31,7 +35,6 @@ export const getThemeModules = (theme = "theme1") => {
         ProductDetail: load(() => import("./theme2/ProductDetail/single/ProductDetail")),
         Login: load(() => import("./theme2/auth/login/page")),
         Signup: load(() => import("./theme2/auth/signup/page")),
-        WholeSaleProductList: load(() => import("./theme1/CategoriesPage/Wholesale/WholeSaleProductList")),
         Catalogue: load(() => import("./theme1/ProductDetail/catalogue/catalogue")),
         BrandList: load(() => import("./theme1/brands/brandLists")),
         WholeSalePage: load(() => import("./theme1/wholesalePage/wholesalepage")),
