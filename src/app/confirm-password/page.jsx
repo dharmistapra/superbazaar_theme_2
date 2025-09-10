bf39d276fd91e51c4122ab786fbca21df7d97ae3
Dharmishtha <dharmistapra.setblue@gmail.com>
2025-09-10 16:04:24 +0530

    Update page.jsx

--- src/app/confirm-password/page.jsx
+++ src/app/confirm-password/page.jsx
@@ -1,7 +1,9 @@
+import { getTheme } from "@/services/layout";
 import { getThemeModules } from "@/theme/themeConfig";
 
-export default function ConfirmPasswordPage() {
-    const currentTheme = process.env.NEXT_THEME || "theme1";
+export default async function ConfirmPasswordPage() {
+    const themeData = await getTheme();
+    const currentTheme = themeData?.name || "theme1";
     const ConfirmPassword = getThemeModules(currentTheme).ConfirmPassword;
     return <ConfirmPassword />;
 }
